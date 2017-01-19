6c8306ca8505f5eaf8d94d16c8be30c9d09dab10
Adrian Frank <adri13@live.de>
2017-01-19 15:56:48 +0100

    added dummy connection setup

--- src/Connector.java
+++ src/Connector.java
@@ -1,9 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Connector {
+    public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        startServer();
+        startSender();
+    }
 
-	}
+    public static void startSender() {
+        (new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Socket s = new Socket("localhost", 60010);
+                    BufferedWriter out = new BufferedWriter(
+                            new OutputStreamWriter(s.getOutputStream()));
 
+                    while (true) {
+                        out.write("Hello World!");
+                        out.newLine();
+                        out.flush();
+
+                        Thread.sleep(200);
+                    }
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public static void startServer() {
+        (new Thread() {
+            @Override
+            public void run() {
+                ServerSocket ss;
+                try {
+                    ss = new ServerSocket(60010);
+
+                    Socket s = ss.accept();
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(s.getInputStream()));
+                    String line = null;
+                    while ((line = in.readLine()) != null) {
+                    	System.out.println("input");
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 }
